582efaed1031e9d68e77fb327ec899b25fbc4b4d
mateus <mateusmmo15@gmail.com>
2021-01-03 23:57:09 -0300

    término do desafio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,16 +11,17 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
-  <Container size={size}>
-    <header>
-      <img src={Logo} alt="GoFinances" />
-      <nav>
-        {
-          // Todo
-        }
-      </nav>
-    </header>
-  </Container>
+  <>
+    <Container size={size}>
+      <header>
+        <img src={Logo} alt="GoFinances" />
+        <nav>
+          <Link to="/">Listagem</Link>
+          <Link to="/import">Importar</Link>
+        </nav>
+      </header>
+    </Container>
+  </>
 );
 
 export default Header;
